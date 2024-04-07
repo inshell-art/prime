251e91cb42b311aaaba9cced730b5cb40c1e2a75
inshell <this.inshell@gmail.com>
2024-04-07 14:03:11 +0800

    ready to dev business logic

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+// Import dependencies
+import functions from "firebase-functions";
+import express from "express";
+
+// Create an Express application
+const app = express();
+
+// Define a route handler
+app.get("/", (req, res) => {
+  res.send("Hello, Firebase with Express and TypeScript!");
+});
+
+// Export the Express app as a Firebase function
+export const api = functions.https.onRequest(app);
